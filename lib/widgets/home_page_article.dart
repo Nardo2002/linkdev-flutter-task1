039bf72c36a13204641cbe5d2f041798ecd90d7d
Nardeen <nardeen.hany.intern@linkdevelopment.com>
2024-07-17 05:48:41 +0300

    added the article details screen

--- lib/widgets/home_page_article.dart
+++ lib/widgets/home_page_article.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/domain/models/article.dart';
+import 'package:news_app/presentation/views/details_view.dart';
 import 'package:intl/intl.dart';
 
 class HomePageArticle extends StatelessWidget {
@@ -16,34 +17,47 @@ class HomePageArticle extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.all(size * 0.05),
-      child: Container(
-        color: Colors.white,
-        child: Column(
-          children: [
-            Image.network(article.urlToImage),
-            Padding(
-              padding: EdgeInsets.all(size * 0.04),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(article.title,
-                      style: TextStyle(
-                          fontSize: size * 0.05,
-                          fontWeight: FontWeight.w300)),
-                  Text('By ${article.author}',
-                      style: TextStyle(
-                          fontSize: size * 0.03,
-                          color: Colors.grey)),
-                  Align(
-                    alignment: Alignment.bottomRight,
-                    child: Text(DateFormat.yMMMMd().format(article.publishedAt), style: TextStyle(
-                            fontSize: size * 0.03,
-                            color: Colors.grey)),
-                  )
-                ],
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => DetailsView(
+                article: article,
+                size: size,
               ),
             ),
-          ],
+          );
+        },
+        child: Container(
+          color: Colors.white,
+          child: Column(
+            children: [
+              Image.network(article.urlToImage),
+              Padding(
+                padding: EdgeInsets.all(size * 0.04),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(article.title,
+                        style: TextStyle(
+                            fontSize: size * 0.05,
+                            fontWeight: FontWeight.w300)),
+                    Text('By ${article.author}',
+                        style: TextStyle(
+                            fontSize: size * 0.03,
+                            color: Colors.grey)),
+                    Align(
+                      alignment: Alignment.bottomRight,
+                      child: Text(DateFormat.yMMMMd().format(article.publishedAt), style: TextStyle(
+                              fontSize: size * 0.03,
+                              color: Colors.grey)),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
